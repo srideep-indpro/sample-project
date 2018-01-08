40351980796b1379e77ddb0363ff3c00ec48869d
ritz890 <srideep58@yahoo.com>
2018-01-08 19:13:28 +0530

    log on console and file after every message sent

---
+++ src/main/java/com/customsecurity/ip/IPAuthenticationPlugin.java
@@ -0,0 +1,23 @@
+package com.customsecurity.ip;
+
+import java.util.List;
+
+import org.apache.activemq.broker.Broker;
+import org.apache.activemq.broker.BrokerPlugin;
+
+public class IPAuthenticationPlugin implements BrokerPlugin {
+	List<String> allowedIPAddresses;
+
+	@Override
+	public Broker installPlugin(Broker broker) throws Exception {
+		return new IPAuthenticationBroker(broker, allowedIPAddresses);
+	}
+
+	public List<String> getAllowedIPAddresses() {
+		return allowedIPAddresses;
+	}
+
+	public void setAllowedIPAddresses(List<String> allowedIPAddresses) {
+		this.allowedIPAddresses = allowedIPAddresses;
+	}
+}
